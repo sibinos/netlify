8865378d83cecef6a4018761e7cb9fc220057fa7
sibinos <sibin.os.g@gmail.com>
2022-11-24 12:46:47 +0530

    final

--- pages/api/auth.js
+++ pages/api/auth.js
@@ -3,23 +3,26 @@ import { randomBytes } from "crypto";
 import { config } from "../../lib/config";
 import { scopes } from "../../lib/scopes";
 
-
 export const randomString = () => randomBytes(4).toString("hex");
 
 export default async (req, res) => {
   const { host } = req.headers;
   const url = new URL(`https://${host}/${req.url}`);
   const urlParams = url.searchParams;
-  const provider= urlParams.get("provider");
+  const provider = urlParams.get("provider");
 
+  // simple-oauth will use our config files to generate a client we can use to request access
   const client = new AuthorizationCode(config(provider));
-
+  
+  // we then make a build the request to our provider
   const authorizationUri = client.authorizeURL({
-    redirect_uri: `https://${host}/callback?provider=${provider}`,
+    // your callback url is important! More on this later
+    redirect_uri: `https://${host}/api/callback?provider=${provider}`,
     scope: scopes[provider],
-    state: randomString(),
+    state: randomString()
   });
 
+  // and get redirected to Github for authorisation
   res.writeHead(301, { Location: authorizationUri });
   res.end();
 };
--- pages/api/callback.js
+++ pages/api/callback.js
@@ -1,43 +1,43 @@
-import { IncomingMessage, ServerResponse } from "http";
 import { AuthorizationCode } from "simple-oauth2";
 import { config } from "../../lib/config";
 
-export default async (req: IncomingMessage, res: ServerResponse) => {
+export default async (req, res) => {
   const { host } = req.headers;
   const url = new URL(`https://${host}/${req.url}`);
   const urlParams = url.searchParams;
   const code = urlParams.get("code");
-  const provider:any = urlParams.get("provider");
+  const provider = urlParams.get("provider");
+  
+  // we recreate the client we used to make the request
   const client = new AuthorizationCode(config(provider));
-  const tokenParams:any = {
+  
+  // create our token object
+  const tokenParams = {
     code,
-    redirect_uri: `https://${host}/callback?provider=${provider}`,
+    redirect_uri: `https://${host}/api/callback?provider=${provider}`
   };
 
   try {
+    // try to create an access token from the client
     const accessToken = await client.getToken(tokenParams);
     const token = accessToken.token["access_token"];
 
     const responseBody = renderBody("success", {
       token,
-      provider,
+      provider
     });
 
     res.statusCode = 200;
     res.end(responseBody);
-  } catch (e:any) {
+  } catch (e) {
     res.statusCode = 200;
     res.end(renderBody("error", e));
   }
 };
 
-function renderBody(
-  status: string,
-  content: {
-    token: string;
-    provider: string;
-  }
-) {
+// This renders a simple page with javascript that allows the pop-up page
+// to communicate with its opener 
+function renderBody(status, content) {
   return `
     <script>
       const receiveMessage = (message) => {
@@ -47,11 +47,9 @@ function renderBody(
   )}',
           message.origin
         );
-
         window.removeEventListener("message", receiveMessage, false);
       }
       window.addEventListener("message", receiveMessage, false);
-
       window.opener.postMessage("authorizing:${content.provider}", "*");
     </script>
   `;
